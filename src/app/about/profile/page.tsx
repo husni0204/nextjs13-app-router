c806ebd3b91dccd441c455b71c3a22d95be05a94
AnakkeWongTani <husnirezky2@gmail.com>
2023-11-17 17:22:07 +0700

    awal layout

--- src/app/about/profile/page.tsx
+++ src/app/about/profile/page.tsx
@@ -1,9 +1,8 @@
-import Navbar from "./navbar"
+import Navbar from "../../navbar"
 
 const ProfilePage = () => {
     return (
       <div>
-        <Navbar />
         <h3>Profile Page</h3>
         </div>
     )
